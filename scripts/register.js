6579958715f96e3d14956a7b8b3112e488376174
joelgonzalez203 <36168549+joelgonzalez203@users.noreply.github.com>
2018-07-19 12:09:25 -0400

    Create register.js

---
+++ scripts/register.js
@@ -0,0 +1,83 @@
+//Used for local testing
+//var baseURL = "http://localhost:8000/API"
+var userID = 0;
+
+function addProfessor() {
+	var name = document.getElementById("form-name").value;
+	var email = document.getElementById("form-email").value;
+	var password = document.getElementById("form-password").value;
+	var passwordConfirm = document.getElementById("form-password-confirm").value;
+	if (password !== passwordConfirm) {
+
+		//document.getElementsByName("response")[0].innerHTML = "Your passwords must match";
+		return;
+	}
+
+	//Maybe hash?
+	//password = md5(password);
+
+	// replace with appropriate varaible names
+	var payload = '{"name" : "' + name + '", "email" : "' + email + '", "password" : "' + password + '"}';
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", baseURL + "/AddProf.php", false);
+	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+    var baseURL = "http://localhost:3306/API"
+    var userID = 0;
+
+    function addProfessor() {
+        var name = document.getElementById("form-name").value;
+        var email = document.getElementById("form-email").value;
+        var password = document.getElementById("form-password").value;
+        var passwordConfirm = document.getElementById("form-password-confirm").value;
+        if (password !== passwordConfirm) {
+            
+            //document.getElementsByName("response")[0].innerHTML = "Your passwords must match";
+            return;
+        }
+
+        //Maybe hash?
+        //password = md5(password);
+
+        // replace with appropriate varaible names
+        var payload = '{"name" : "' + name + '", "email" : "' + email + '", "password" : "' + password + '"}';
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", baseURL + "/AddProf.php", false);
+        xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+        try {
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState === 4){
+                    var data = JSON.parse(xhr.responseText);
+                    var error = data.error;
+
+                    window.location.href = "Login.html";
+                }
+            }
+
+            xhr.send(payload);
+        }
+        catch(error) {
+
+        }
+        return false;
+    }
+
+	try {
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState === 4){
+				var data = JSON.parse(xhr.responseText);
+				var error = data.error;
+
+				window.location.href = "Login.html";
+			}
+		}
+
+		xhr.send(payload);
+	}
+	catch(error) {
+
+	}
+	return false;
+}
