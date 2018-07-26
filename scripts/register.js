377639ed7410d5e1a11fd237c301a536bb223ce5
joelgonzalez203 <36168549+joelgonzalez203@users.noreply.github.com>
2018-07-26 15:26:31 -0400

    Update register.js

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,5 @@
 //var baseURL = "http://localhost:8000/API"
-var baseURL = "http://group5.gearhostpreview.com/originalDevSetup/API";
+var baseURL = "http://newlook.gearhostpreview.com/originalDevSetup/API";
 var userID = 0;
 
 function addProfessor() {
